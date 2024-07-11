bd91fd815765c95c73a7cc719e8aebfeda855ace
Dm&try <dmitrypanyukov@gmail.com>
2024-07-12 01:41:21 +0300

    Start Spring

---
+++ src/main/java/com/vidjaolcalculator/vidjaolcalculatorspringdemo/VidjaolcalculatorSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vidjaolcalculator.vidjaolcalculatorspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidjaolcalculatorSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidjaolcalculatorSpringDemoApplication.class, args);
+	}
+
+}
